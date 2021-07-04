568ccbead966eddb28c3763e8711fbbe52735012
Get Shwifty <cjpatr0n1te@icloud.com>
2021-07-03 21:06:59 -0700

    added function that generates and displays pw

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment code here
 
-let acceptLength = function() {
+
+let acceptLength = function () {
   let formatString = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 
   let promptLength = window.prompt('Please enter length of password, length can be between 8 to 128 characters');
@@ -8,12 +9,11 @@ let acceptLength = function() {
   let alphaNumStr = (/[A-Za-z]/).test(promptLength);
   let specialChar = formatString.test(promptLength);
 
-  while(promptLength===null) {
+  while (promptLength === null) {
     return 1;
   }
 
-  if  (parseInt(promptLength) < 8 || parseInt(promptLength) > 128 || promptLength === "" || alphaNumStr || specialChar)
-  {
+  if (parseInt(promptLength) < 8 || parseInt(promptLength) > 128 || promptLength === "" || alphaNumStr || specialChar) {
     alert('Please enter a number between 8 and 128.');
     return 0;
   }
@@ -21,11 +21,11 @@ let acceptLength = function() {
   return promptLength;
 };
 
-let charType = function() {
-  let charType = prompt('Please enter the character type, select from the following (1 or 2):\n'+
-                        '1: lowercase, uppercase letters and numbers/n'+
-                        '2: lowercase, uppercase letters, numbers and special characters ');
-                      
+let charType = function () {
+  let charType = prompt('Please enter the character type, select from the following (1 or 2):\n' +
+    '1: lowercase, uppercase letters and numbers/n' +
+    '2: lowercase, uppercase letters, numbers and special characters ');
+
   while (charType === null) {
     return 1;
   }
@@ -48,16 +48,45 @@ let charType = function() {
   }
 };
 
+let passwordGen = function () {
+  var generatedPass = "";
+  let pwLength = acceptLength();
+
+  if (parseInt(pwLength) !== 1) {
+    while (pwLength === 0) {
+      pwLength = acceptLength();
+    }
+  };
+
+  if (parseInt(pwLength) !== 1) {
+    let passString = charType();
+
+    if (parseInt(passString) !== 1) {
+      while (passString === 0) {
+        passString = charType;
+      }
+    }
+
+    for (let i = 0; i < pwLength; i++) {
+      let position = Math.floor(Math.random() * passString.length);
+      var generatedPass = generatedPass + passString.charAt(position);
+    }
+  };
+
+  return generatedPass;
+};
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = passwordGen();
+  if (password != "") {
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+    passwordText.value = password;
+  }
 }
 
 // Add event listener to generate button
